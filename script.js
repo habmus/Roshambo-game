b2a753d589d02ba1270a0232f39b15f1b068305b
habmus <145584492+habmus@users.noreply.github.com>
2024-03-02 04:45:46 -0600

    Finished Part1 of Roshambo

--- script.js
+++ script.js
@@ -2,5 +2,45 @@ function getComputerChoice(){
     let array = ["Rock","Paper","Scissors"];
     let randomValue = array[Math.floor(Math.random() * array.length)];
     return randomValue;
-}
-console.log(getComputerChoice());
+  }
+  
+  function playRound(playerSelection, computerSelection){
+    if(playerSelection.toLowerCase() === computerSelection.toLowerCase()){
+      console.log(playerSelection.toLowerCase())
+      console.log(computerSelection.toLowerCase())
+      return "DRAW"
+    }else if(playerSelection.toLowerCase() === "rock" && computerSelection.toLowerCase() === "paper"){
+      console.log(playerSelection.toLowerCase())
+      console.log(computerSelection.toLowerCase())
+      return "You lose! Paper beats Rock";
+    }else if(playerSelection.toLowerCase() ==="paper" && computerSelection.toLowerCase() === "scissors"){
+      console.log(playerSelection.toLowerCase())
+      console.log(computerSelection.toLowerCase())
+      return "You lose! Scissors beats Paper";
+    }else if(playerSelection.toLowerCase() ==="scissors" && computerSelection.toLowerCase() === "rock"){
+      console.log(playerSelection.toLowerCase())
+      console.log(computerSelection.toLowerCase())
+      return "You lose! Rock beats Scissors";
+    }else if(playerSelection.toLowerCase() !== "rock" && playerSelection.toLowerCase() !== "paper" && playerSelection.toLowerCase() !== "scissors"){
+      console.log(playerSelection.toLowerCase())
+      console.log(computerSelection.toLowerCase())
+      return "Invalid";
+    }else{
+      console.log(playerSelection.toLowerCase())
+      console.log(computerSelection.toLowerCase())
+      return "You win!!!!!!!!!!"
+    }
+  }
+  
+  
+  function playGame(){
+      for(i = 0; i<5; i++){
+      const playerSelection = prompt();
+      const computerSelection = getComputerChoice();
+      console.log(playRound(playerSelection, computerSelection));
+    }
+  }
+  
+  playGame();
+  
+  
